a8a34ba7212bc7c07cb36e503217486ca04ef482
Taras Rushchak <taraskhust@gmail.com>
2019-06-20 10:07:58 +0300

    add method item create

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import Router from 'next/router';
 import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
 import multiUserInput from '../lib/reducer/multiUserInput';
+import Error from './ErrorMessage';
+
+const CREATE_ITEM_MUTATION = gql`
+mutation CREATE_ITEM_MUTATION(
+		$title: String!
+	  $description: String!
+	  $image: String
+	  $largeImage: String
+	  $price: Int!
+	  ) 
+  {
+		createItem(
+        title: $title
+        description: $description
+        image: $image
+        largeImage: $largeImage
+        price: $price
+				
+		) {
+			id
+}
+}
+`;
 
 const CreateItem = () => {
 	const initialValue = {
@@ -16,48 +40,59 @@ const CreateItem = () => {
 	const [userInput, handleChange] = multiUserInput(initialValue);
 	const { title, price, description } = userInput;
 	return (
-			<Form onSubmit={(e) => {
-				e.preventDefault()
-				console.log(userInput)
-			}}>
-				<fieldset>
-					<label htmlFor="title">
-						Title
-						<input type="text"
-						       id="title"
-						       name="title"
-						       placeholder="Title"
-						       value={title}
-						       required={title}
-						       onChange={handleChange}
-						/>
-					</label>
-					<label htmlFor="price">
-						Price
-						<input type="number"
-						       id="price"
-						       name="price"
-						       placeholder="Price"
-						       value={price}
-						       required={price}
-						       onChange={handleChange}
-						/>
-					</label>
-					<label htmlFor="description">
-						Description
-						<textarea type="text"
-						       id="description"
-						       name="description"
-						       placeholder="Description"
-						       value={description}
-						       required={description}
-						       onChange={handleChange}
-						/>
-					</label>
-					<button type="submit">Submit</button>
-				</fieldset>
-			</Form>
+			<Mutation mutation={CREATE_ITEM_MUTATION} variables={userInput}>
+				{(createItem, { loading, error }) => (
+						<Form onSubmit={async e => {
+							e.preventDefault();
+							const res = await createItem();
+							Router.push({
+								pathname: '/items',
+								query: { id: res.data.createItem.id },
+							});
+						}}>
+							<Error error={error}/>
+							<fieldset disabled={loading} aria-busy={loading}>
+								<label htmlFor="title">
+									Title
+									<input type="text"
+									       id="title"
+									       name="title"
+									       placeholder="Title"
+									       value={title}
+									       required={title}
+									       onChange={handleChange}
+									/>
+								</label>
+								<label htmlFor="price">
+									Price
+									<input type="number"
+									       id="price"
+									       name="price"
+									       placeholder="Price"
+									       value={price}
+									       required={price}
+									       onChange={handleChange}
+									/>
+								</label>
+								<label htmlFor="description">
+									Description
+									<textarea type="text"
+									          id="description"
+									          name="description"
+									          placeholder="Description"
+									          value={description}
+									          required={description}
+									          onChange={handleChange}
+									/>
+								</label>
+								<button type="submit">Submit</button>
+							</fieldset>
+						</Form>
+				)
+				}
+			</Mutation>
 	);
 };
 
 export default CreateItem;
+export { CREATE_ITEM_MUTATION };
