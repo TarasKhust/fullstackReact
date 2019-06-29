e8b7d8268795b02a1ce12b547cd4bfb1fc0298f3
Taras Rushchak <taraskhust@gmail.com>
2019-06-29 18:52:02 +0300

    createitem sigin

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,12 +1,12 @@
-import React from 'react';
 import CreateItem from '../components/CreateItem';
+import PleaseSignIn from '../components/PleaseSignIn';
 
-const Sell = (props) => {
-	return (
-			<div>
-				<CreateItem/>
-			</div>
-	);
-};
+const Sell = props => (
+	<div>
+		<PleaseSignIn>
+			<CreateItem />
+		</PleaseSignIn>
+	</div>
+);
 
 export default Sell;
